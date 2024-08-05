da20d53d7ecd8024f9fa2bb37e6d81806417ef6d
imzisy <izaibak@gmail.com>
2024-08-05 22:38:53 +0800

    update

--- consumer/src/App.vue
+++ consumer/src/App.vue
@@ -4,7 +4,7 @@ export default {
 };
 </script>
 <template>
-  <div :class="$style.view">
+  <div>
     Hi from consumer test
     <div>
       <router-view />
@@ -13,9 +13,6 @@ export default {
 </template>
 
 <style module scoped>
-.view {
-  @apply min-h-screen overflow-hidden bg-gray-100 print: print:invisible print:h-full;
-}
 
 .gridView {
   @apply grid grid-cols-1 gap-4 md:grid-cols-3;
